65c07dc68907ffbc9086287482f654ab16182918
Chih Cheng Liang <chihchengliang@gmail.com>
2020-05-23 18:07:13 +0800

    all test fixed

--- plonk/constraint.py
+++ plonk/constraint.py
@@ -1,11 +1,13 @@
 def constraint_polynomial(Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci):
-    return(Qli*ai + Qri*bi + Qoi*ci + Qmi*ai*bi + Qci == 0)
+    return Qli * ai + Qri * bi + Qoi * ci + Qmi * ai * bi + Qci == 0
+
 
 def is_satisfied(Ql, Qr, Qm, Qo, Qc, a, b, c):
-    for Qli,Qri,Qmi,Qoi,Qci,ai,bi,ci in zip (Ql,Qr,Qm,Qo,Qc,a,b,c):
-        if (constraint_polynomial(Qli,Qri,Qmi,Qoi,Qci,ai,bi,ci) == False):
-            return(False)
-    return(True)
+    for Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci in zip(Ql, Qr, Qm, Qo, Qc, a, b, c):
+        if constraint_polynomial(Qli, Qri, Qmi, Qoi, Qci, ai, bi, ci) == False:
+            return False
+    return True
+
 
 def add_add_constarint(Ql, Qr, Qm, Qo, Qc):
     Ql.append(1)
@@ -13,7 +15,8 @@ def add_add_constarint(Ql, Qr, Qm, Qo, Qc):
     Qm.append(0)
     Qo.append(-1)
     Qc.append(0)
-    return(Ql, Qr, Qm, Qo, Qc)
+    return (Ql, Qr, Qm, Qo, Qc)
+
 
 def add_mul_constarint(Ql, Qr, Qm, Qo, Qc):
 
@@ -23,7 +26,8 @@ def add_mul_constarint(Ql, Qr, Qm, Qo, Qc):
     Qo.append(-1)
     Qc.append(0)
 
-    return(Ql, Qr, Qm, Qo, Qc)
+    return (Ql, Qr, Qm, Qo, Qc)
+
 
 def add_constant_constraint(Ql, Qr, Qm, Qo, Qc, const):
 
@@ -33,6 +37,4 @@ def add_constant_constraint(Ql, Qr, Qm, Qo, Qc, const):
     Qo.append(0)
     Qc.append(-const)
 
-    return(Ql, Qr, Qm, Qo, Qc)
-
-
+    return (Ql, Qr, Qm, Qo, Qc)
