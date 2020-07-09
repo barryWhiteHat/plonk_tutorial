d0b00cca30a88b811d4cb25df93f06b1e3e988e8
Thore <hildebrandtthore@gmail.com>
2020-07-09 16:55:42 +1200

    Committing all changes to the copy constraints section at once

--- plonk/copy_constraint.py
+++ plonk/copy_constraint.py
@@ -1,4 +1,4 @@
-from .poly import lagrange, polynomial_eval
+from .poly import polynomial_eval
 
 
 def copy_constraint_simple(eval_domain, Xcoef, Ycoef, v1, v2):
@@ -9,7 +9,7 @@ def copy_constraint_simple(eval_domain, Xcoef, Ycoef, v1, v2):
 
     for i in range(0, len(eval_domain)):
         x.append(polynomial_eval(Xcoef, eval_domain[i]))
-        Y.append(polynomial_eval(Ycoef, x[i]))
+        Y.append(polynomial_eval(Ycoef, eval_domain[i]))
 
         rlc.append(v1 + x[i] + v2 * Y[i])
         Px.append(Px[i] * (v1 + x[i] + v2 * Y[i]))
@@ -17,8 +17,14 @@ def copy_constraint_simple(eval_domain, Xcoef, Ycoef, v1, v2):
     return (x, Y, Px, rlc)
 
 
-def find_permutation(copies, eval_domain):
-
-    perm = lagrange(eval_domain, copies)
-    perm = [float(x) for x in reversed(perm.coefficients)]
-    return perm
+def find_permutation(wires):
+    # This function takes an array "wires" of arbitrary values and returns an
+    # array with shuffles the indices of "wires" for repeating values
+    size = len(wires)
+    permutation = [i for i in range(size)]
+    for i in range(size):
+        for j in range(i + 1, size):
+            if wires[i] == wires[j]:
+                permutation[i], permutation[j] = permutation[j], permutation[i]
+                break
+    return permutation
--- plonk/plonk.py
+++ plonk/plonk.py
@@ -5,38 +5,38 @@ import pdb
 
 
 def verify_naieve(Ql, Qr, Qm, Qo, Qc, a, b, c, witness_y, perm_a, perm_b, perm_c):
-
     # make sure constarints + witness is statisfied
     assert is_satisfied(Ql, Qr, Qm, Qo, Qc, a, b, c) == True
 
     # original polynomial
 
-    Xcoef = [0, 1]
+    Xcoef_a = [0, 1]
+    Xcoef_b = [len(a), 1]
+    Xcoef_c = [2 * len(a), 1]
 
     # todo: replace with random
 
     v1 = hash(str(a + b + c))
     v2 = hash(str(c + a + b))
 
+    a_poly = gen_poly(range(0, len(a)), a)
+    b_poly = gen_poly(range(0, len(a)), b)
+    c_poly = gen_poly(range(0, len(a)), c)
     eval_domain = range(0, len(a) * 3)
 
-    x, Y, Px_a, rlc = copy_constraint_simple(range(0, len(a)), Xcoef, witness_y, v1, v2)
-    x, Y, Px_b, rlc = copy_constraint_simple(
-        range(len(a), len(a) * 2), Xcoef, witness_y, v1, v2
-    )
-    x, Y, Px_c, rlc = copy_constraint_simple(
-        range(len(a) * 2, len(a) * 3), Xcoef, witness_y, v1, v2
-    )
+    x, Y, Px_a, rlc = copy_constraint_simple(range(0, len(a)), Xcoef_a, a_poly, v1, v2)
+    x, Y, Px_b, rlc = copy_constraint_simple(range(0, len(a)), Xcoef_b, b_poly, v1, v2)
+    x, Y, Px_c, rlc = copy_constraint_simple(range(0, len(a)), Xcoef_c, c_poly, v1, v2)
 
     # calcualte permutated polynomial
     x_1, Y_1, Px_a_prime, rlc_1 = copy_constraint_simple(
-        range(0, len(a)), perm_a, witness_y, v1, v2
+        range(0, len(a)), perm_a, a_poly, v1, v2
     )
     x_1, Y_1, Px_b_prime, rlc_1 = copy_constraint_simple(
-        range(len(a), len(a) * 2), perm_b, witness_y, v1, v2
+        range(0, len(a)), perm_b, b_poly, v1, v2
     )
     x_1, Y_1, Px_c_prime, rlc_1 = copy_constraint_simple(
-        range(len(a) * 2, len(a) * 3), perm_c, witness_y, v1, v2
+        range(0, len(a)), perm_c, c_poly, v1, v2
     )
 
     assert (
@@ -56,7 +56,7 @@ def verify_naieve(Ql, Qr, Qm, Qo, Qc, a, b, c, witness_y, perm_a, perm_b, perm_c
 
 
 def create_proof(a, b, c):
-
-    poly = gen_poly(a + b + c)
+    domain = range(0, len(a) * 3)
+    poly = gen_poly(domain, a + b + c)
 
     return (a, b, c, poly)
